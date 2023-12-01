a18cc7846df9d652d9288262a945f835c0293bca
Lemibuca <146123937+Lemibuca@users.noreply.github.com>
2023-11-30 23:58:33 -0500

    Python Queue

---
+++ PythonQueue.py
@@ -0,0 +1,14 @@
+names = []
+
+namecount = 0
+for x in range (10):
+    name = input("Enter name: ")
+    names.append (name)
+    namecount +=1
+
+
+print (names)
+
+for x in range (10):
+    len(name)
+    print(names.pop(0))
